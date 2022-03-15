53eef4e4a010c0ff360a04fe123b3bbbf4d59abb
Accurics <admin@accurics.com>
2022-03-15 12:31:16 +0530

    Auto Remediation by Accurics, file: unknown

--- main-aws.tf
+++ main-aws.tf
@@ -79,3 +79,12 @@ resource "aws_s3_bucket_policy" "my_vpc" {
 }
 POLICY
 }
+resource "aws_s3_bucket_server_side_encryption_configuration" "example" {
+  bucket = aws_s3_bucket.my_vpc_flow_log_s3_bucket.bucket
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "aws:kms"
+    }
+  }
+}
