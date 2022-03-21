ce7fc78cb288ec5149b4db8f31100331a0a80fca
Accurics <admin@accurics.com>
2022-03-21 21:04:34 +0530

    Auto Remediation by Accurics, file: blob/main/unknown

--- blob/main/main-aws.tf
+++ blob/main/main-aws.tf
@@ -8,3 +8,12 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "example" {
     }
   }
 }
+resource "aws_s3_bucket_server_side_encryption_configuration" "example" {
+  bucket = aws_s3_bucket.my_vpc_flow_log_s3_bucket.bucket
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "aws:kms"
+    }
+  }
+}
